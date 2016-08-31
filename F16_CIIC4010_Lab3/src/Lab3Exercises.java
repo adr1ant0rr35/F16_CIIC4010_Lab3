f81b888888d27716703e6cc4d3be3ae79c37a1b2
4015c23 <4015c23@mamey.ece.uprm.edu>
2016-08-31 13:19:23 -0400

    Changes made to the classes

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -1,11 +1,15 @@
 import javax.swing.JFrame;
 
-public class Lab3Exercises {
-	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
+public class Lab3Exercises 
+{
+	public static void main(String[] args) 
+	{
+		JFrame myFrame = new JFrame("Adrian Torres");
 		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		myFrame.setSize(200, 200);
+		MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
 	}
 }
